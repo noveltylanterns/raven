2cd54b0055f77f872daae71711f6c3098b0aac21
HumphreyBoaGart <donotreply@lanterns.io>
2026-03-24 22:48:57 -0500

    patch installer

--- public/install.php
+++ public/install.php
@@ -122,12 +122,12 @@ function installer_start_session(): void
     @ini_set('session.use_cookies', '1');
     @ini_set('session.use_only_cookies', '1');
     @ini_set('session.use_trans_sid', '0');
+    @ini_set('session.cookie_domain', '');
 
     session_name(installer_session_name());
     session_set_cookie_params([
         'lifetime' => 0,
         'path' => installer_session_cookie_path(),
-        'domain' => '',
         'secure' => (!empty($_SERVER['HTTPS']) && strtolower((string) $_SERVER['HTTPS']) !== 'off'),
         'httponly' => true,
         'samesite' => 'Lax',
